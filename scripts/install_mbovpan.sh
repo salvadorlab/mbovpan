e3eed3f7a98fc0c1b6df1b343575e97ab33842e1
Noah Legall <noahaus@uga.edu>
2023-04-25 18:10:16 -0400

    make install file

---
+++ scripts/install_mbovpan.sh
@@ -0,0 +1,11 @@
+conda create -n mbovpan
+conda activate mbovpan
+conda install -c bioconda -c conda-forge -c defaults nextflow
+conda install -c bioconda -c conda-forge -c defaults fastp
+conda install -c bioconda fastqc
+conda install -c conda-forge -c bioconda -c defaults samtools
+conda install -c conda-forge -c bioconda -c defaults picard
+conda install -c conda-forge -c bioconda -c defaults bwa
+conda install -c conda-forge -c bioconda -c defaults biopython
+conda install -c conda-forge -c bioconda -c defaults freebayes
+conda install -c conda-forge -c bioconda -c defaults spades
