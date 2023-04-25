420c0803b6b6e1d21e7d5dade0192cc0e9bb21bb
Noah Legall <noahaus@uga.edu>
2023-04-25 18:16:17 -0400

    updating the install

--- scripts/install_mbovpan.sh
+++ scripts/install_mbovpan.sh
@@ -1,11 +1,10 @@
-conda create -n mbovpan
-conda activate mbovpan
-conda install -c bioconda -c conda-forge -c defaults nextflow
-conda install -c bioconda -c conda-forge -c defaults fastp
-conda install -c bioconda fastqc
-conda install -c conda-forge -c bioconda -c defaults samtools
-conda install -c conda-forge -c bioconda -c defaults picard
-conda install -c conda-forge -c bioconda -c defaults bwa
-conda install -c conda-forge -c bioconda -c defaults biopython
-conda install -c conda-forge -c bioconda -c defaults freebayes
-conda install -c conda-forge -c bioconda -c defaults spades
+source activate mbovpan
+conda install -y -c bioconda -c conda-forge -c defaults nextflow
+conda install -y -c bioconda -c conda-forge -c defaults fastp
+conda install -y -c bioconda fastqc
+conda install -y -c conda-forge -c bioconda -c defaults samtools
+conda install -y -c conda-forge -c bioconda -c defaults picard
+conda install -y -c conda-forge -c bioconda -c defaults bwa
+conda install -y -c conda-forge -c bioconda -c defaults biopython
+conda install -y -c conda-forge -c bioconda -c defaults freebayes
+conda install -y -c conda-forge -c bioconda -c defaults spades
