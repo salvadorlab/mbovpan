c833d49d0d188992a1e0b58bf17537bde962b207
Noah Legall <40697188+noahaus@users.noreply.github.com>
2023-05-03 14:59:28 -0400

    try read.csv2

--- scripts/gene_prab.R
+++ scripts/gene_prab.R
@@ -15,7 +15,7 @@ args = commandArgs(trailingOnly=TRUE)
 # load in the gene presence absence data, keep only accessory
 # we should already have access to this in our directory
 print(args[1])
-gene_pres_abs <- read.csv(args[1], header = TRUE, stringsAsFactors = FALSE, row.names = "Gene")
+gene_pres_abs <- read.csv2(args[1], header = TRUE, stringsAsFactors = FALSE, row.names = "Gene")
 accessory_genome <- gene_pres_abs[!(is.na(gene_pres_abs$Accessory.Fragment)),]
 core_genome <- gene_pres_abs[is.na(gene_pres_abs$Accessory.Fragment),]
 auxil <- gene_pres_abs %>% select(2:14)
