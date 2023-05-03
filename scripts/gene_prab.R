ad3904340f1277a756d25ae9dc793388ad9504d5
Noah Legall <40697188+noahaus@users.noreply.github.com>
2023-05-03 15:36:02 -0400

    changing the variable

--- scripts/gene_prab.R
+++ scripts/gene_prab.R
@@ -10,13 +10,14 @@ library(ggnewscale)
 
 # load in the general metadata
 args = commandArgs(trailingOnly=TRUE)
-print("testing")
-gene_pres_abs <- read.csv('mbov_virulent_prab.csv', header = TRUE, stringsAsFactors = FALSE, row.names = "Gene")
+#print("testing")
+#gene_pres_abs <- read.csv('mbov_virulent_prab.csv', header = TRUE, stringsAsFactors = FALSE, row.names = "Gene")
 
 # load in the gene presence absence data, keep only accessory
 # we should already have access to this in our directory
 print(args[1])
-gene_pres_abs <- read.csv(args[1], header = TRUE, stringsAsFactors = FALSE, row.names = "Gene")
+test = args[1]
+gene_pres_abs <- read.csv(test, header = TRUE, stringsAsFactors = FALSE, row.names = "Gene")
 accessory_genome <- gene_pres_abs[!(is.na(gene_pres_abs$Accessory.Fragment)),]
 core_genome <- gene_pres_abs[is.na(gene_pres_abs$Accessory.Fragment),]
 auxil <- gene_pres_abs %>% select(2:14)
