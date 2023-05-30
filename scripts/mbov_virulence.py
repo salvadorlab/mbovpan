47a84c8cbde14f2497ab3b38c9d6a9ffa9c1e4ee
Noah Legall <noahaus@uga.edu>
2023-05-29 21:50:30 -0400

    generating print statements

--- scripts/mbov_virulence.py
+++ scripts/mbov_virulence.py
@@ -5,6 +5,8 @@ import sys
 mbov_prab = pd.read_csv(sys.argv[1],low_memory=False)
 mbov_virulence = pd.read_csv(sys.argv[2])
 
+print("files read successfully")
+
 # extract virulence genes as a list of unique elements
 virulent_gene_list = list(set(mbov_virulence.loc[:,"Accronyme"].to_list()))
 
@@ -13,3 +15,4 @@ filtered_list = [ i for i in range(len(mbov_prab.index)) if str(mbov_prab.loc[i,
 
 #filter the rows with the passing indicies
 mbov_prab.iloc[filtered_list].to_csv("mbov_virulent_prab.csv",index=False)
+print("prab created successfully")
