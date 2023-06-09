63a11f6ee9b9fba4afdbc5a2eb3e7a9849a2204a
Noah Legall <noahaus@uga.edu>
2023-06-09 10:25:10 -0400

    check if the function matters

--- scripts/gene_prab.R
+++ scripts/gene_prab.R
@@ -51,7 +51,7 @@ ad_gg[["data"]]$label <- gsub(".annot","",ad_gg$data$label)
 # check if the dendrogram was made accordingly based on the data present
 #print(ad_gg[["data"]])
 
-pdf("gene_prab_figures.pdf")
+png("gene_prab_figures.png")
 
  isolate_dat <- read.csv(args[1], stringsAsFactors = FALSE, check.names = FALSE)
  ad_gg <- ad_gg %<+% isolate_dat +
