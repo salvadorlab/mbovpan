3725ad9b0f90b55ad73c717872e09e2830fbcae2
Noah Legall <noahaus@uga.edu>
2023-06-09 10:00:54 -0400

    Adding time to the figure

--- scripts/gene_prab.R
+++ scripts/gene_prab.R
@@ -81,7 +81,7 @@ ad_gg[["data"]]$label <- gsub(".annot","",ad_gg$data$label)
       t1_scaled <- t1 + new_scale_fill()
       t2 <- gheatmap(t1_scaled,accessory_transpose, offset = 3, colnames_angle=45,hjust = 1,colnames_offset_y = -2.5,font.size = 3) +  
         scale_fill_manual(values = c("gray75","darkblue"), name = "Presence/Absence")
-      Sys.sleep(1) #gives program the time to make the figure
+      Sys.sleep(5) #gives program the time to make the figure
       print(t2[["data"]])
       return(t2)
       
