91530761d570e22082813363ec5c368c304b5e48
Noah Legall <40697188+noahaus@users.noreply.github.com>
2023-07-07 17:54:46 +0000

    removing mamba for now

---
+++ scripts/install_mbovpan.sh
@@ -0,0 +1,10 @@
+source activate mbovpan
+conda install -y -c bioconda -c conda-forge -c defaults nextflow=22.10.6
+conda install -y -c bioconda -c conda-forge -c defaults fastp
+conda install -y -c bioconda fastqc
+conda install -y -c conda-forge -c bioconda -c defaults samtools
+conda install -y -c conda-forge -c bioconda -c defaults picard
+conda install -y -c conda-forge -c bioconda -c defaults bwa
+conda install -y -c conda-forge -c bioconda -c defaults biopython
+conda install -y -c conda-forge -c bioconda -c defaults freebayes
+conda install -y -c conda-forge -c bioconda -c defaults spades
