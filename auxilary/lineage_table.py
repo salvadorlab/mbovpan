39d571c037250bd62eceb2d192af6a37c73c6779
Noah Legall <noahaus@uga.edu>
2023-10-25 22:01:35 -0400

    added read_json, then print the contents

--- auxilary/lineage_table.py
+++ auxilary/lineage_table.py
@@ -2,9 +2,11 @@
 import pandas as pd
 import sys
 
-lineage_csv = pd.read_csv(sys.argv[1])
+lineage_csv = pd.read_json(sys.argv[1])
 spotyping_csv = pd.read_csv(sys.argv[2])
 
 print("files read successfully")
+print(lineage_csv)
+print(spotyping_csv)
 
 
