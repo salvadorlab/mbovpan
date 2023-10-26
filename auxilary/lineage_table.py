73a3b55062107dd61374e20f8fd690502482d6a4
Noah Legall <noahaus@uga.edu>
2023-10-25 22:30:22 -0400

    reading the json files

--- auxilary/lineage_table.py
+++ auxilary/lineage_table.py
@@ -7,6 +7,7 @@ import glob
 jsons = glob.glob("*.results.json")
 
 for j in jsons:
+    print(j)
     data = json.load(j)
 
     for i in data['id']:
@@ -14,10 +15,10 @@ for j in jsons:
 
 
 # lineage_csv = pd.read_json(sys.argv[1], lines=True, encoding='utf-8-sig')
-spotyping_csv = pd.read_csv(sys.argv[2])
+spotyping_csv = pd.read_csv(sys.argv[1])
 
 print("files read successfully")
-print(lineage_csv)
+#print(lineage_csv)
 print(spotyping_csv)
 
 
