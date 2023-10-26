ac30071046c28850f8c1f218eddbe58c42264a8f
Noah Legall <noahaus@uga.edu>
2023-10-25 22:18:44 -0400

    loading csv with json module

--- auxilary/lineage_table.py
+++ auxilary/lineage_table.py
@@ -1,8 +1,16 @@
 # Return a filtered PRAB matrix
 import pandas as pd
 import sys
+import json
 
-lineage_csv = pd.read_json(sys.argv[1], lines=True, encoding='utf-8-sig')
+lineage_csv = open(sys.argv[1])
+data = json.load(lineage_csv)
+
+for i in data['id']:
+    print(i)
+
+
+# lineage_csv = pd.read_json(sys.argv[1], lines=True, encoding='utf-8-sig')
 spotyping_csv = pd.read_csv(sys.argv[2])
 
 print("files read successfully")
