4481ef2c431dc09f56cf6ca384e518f5c1d4840e
Noah Legall <noahaus@uga.edu>
2024-01-06 14:54:10 -0500

    check output of mbov_virulence

--- scripts/mbov_virulence.py
+++ scripts/mbov_virulence.py
@@ -9,10 +9,14 @@ print("files read successfully")
 
 # extract virulence genes as a list of unique elements
 virulent_gene_list = list(set(mbov_virulence.loc[:,"Accronyme"].to_list()))
+print(virulent_gene_list)
 
 #return only rows that have the gene name matching in the annotation!
-filtered_list = [ i for i in range(len(mbov_prab.index)) if str(mbov_prab.loc[i,"Gene"]).split("_")[0] in '\t'.join(virulent_gene_list)]
-
+filtered_list = [] 
+for i in range(len(mbov_prab.index)):
+    print(mbov_prab.loc[i,"Gene"])
+    if str(mbov_prab.loc[i,"Gene"]).split("_")[0] in '\t'.join(virulent_gene_list):
+        filtered_list.append(i)
 #filter the rows with the passing indicies
 mbov_prab.iloc[filtered_list].to_csv("mbov_virulent_prab.csv",index=False)
 print("prab created successfully")
