574095ef6400ce96ec48a9b211e387bed5e5c585
Noah Legall <noahaus@uga.edu>
2024-01-06 16:01:54 -0500

    clean up the gene names

--- scripts/mbov_virulence.py
+++ scripts/mbov_virulence.py
@@ -19,6 +19,7 @@ for i in range(len(mbov_prab.index)):
     for gene in mbov_prab.loc[i,"Gene"].split("~~~"):
         if gene.split("_")[0] in virulent_gene_list:
             filtered_list.append(i)
+            mbov_prab.loc[i,"Gene"] = gene.split("_")[0]
             print("part of the list")
 #filter the rows with the passing indicies
 mbov_prab.iloc[filtered_list].drop_duplicates().to_csv("mbov_virulent_prab.csv",index=False)
