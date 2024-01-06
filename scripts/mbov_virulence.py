58b235a31d4cfe6c416a3f7c3d6a7d31dae82b45
Noah Legall <noahaus@uga.edu>
2024-01-06 16:08:15 -0500

    quick hack

--- scripts/mbov_virulence.py
+++ scripts/mbov_virulence.py
@@ -12,15 +12,18 @@ virulent_gene_list = list(set(mbov_virulence.loc[:,"Accronyme"].to_list()))
 
 #return only rows that have the gene name matching in the annotation!
 filtered_list = [] 
+j = 0
 for i in range(len(mbov_prab.index)):
     #print(mbov_prab.loc[i,"Gene"])
     #print(mbov_prab.loc[i,"Gene"].split("_"))
     #print(mbov_prab.loc[i,"Gene"].split("_")[0])
+    j = i
     for gene in mbov_prab.loc[i,"Gene"].split("~~~"):
         if gene.split("_")[0] in virulent_gene_list:
-            filtered_list.append(i)
-            mbov_prab.loc[i,"Gene"] = gene.split("_")[0]
+            filtered_list.append(j)
+            mbov_prab.loc[j,"Gene"] = gene.split("_")[0]
             print("part of the list")
+        j += 1
 #filter the rows with the passing indicies
 mbov_prab.iloc[filtered_list].drop_duplicates().to_csv("mbov_virulent_prab.csv",index=False)
 print(mbov_prab.iloc[filtered_list])
