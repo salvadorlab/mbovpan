0cff875f9fb486a06090d43ce9f87643a447ee44
Noah Legall <noahaus@uga.edu>
2024-01-06 15:18:36 -0500

    finer grain analysis

--- scripts/mbov_virulence.py
+++ scripts/mbov_virulence.py
@@ -14,9 +14,12 @@ print(virulent_gene_list)
 #return only rows that have the gene name matching in the annotation!
 filtered_list = [] 
 for i in range(len(mbov_prab.index)):
-    #print(mbov_prab.loc[i,"Gene"])
+    print(mbov_prab.loc[i,"Gene"])
+    print(mbov_prab.loc[i,"Gene"].split("_"))
+    print(mbov_prab.loc[i,"Gene"].split("_")[0])
     if str(mbov_prab.loc[i,"Gene"]).split("_")[0] in '\t'.join(virulent_gene_list):
         filtered_list.append(i)
+        print("part of the list")
 #filter the rows with the passing indicies
 mbov_prab.iloc[filtered_list].to_csv("mbov_virulent_prab.csv",index=False)
 print("prab created successfully")
